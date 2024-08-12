24b6bbf313b93d7f8c092a5d5a46db8c641408a1
victorvirgolino <victorvirgolino@gmail.com>
2024-08-12 10:25:43 -0300

    add realizarPix

---
+++ src/main/java/acc/br/accenturebank/dto/conta/PixDTO.java
@@ -0,0 +1,19 @@
+package acc.br.accenturebank.dto.conta;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class PixDTO {
+
+    @NotBlank(message = "A chave não pode ser nula.")
+    private String chave;
+
+    @NotNull(message = "O Valor não pode ser nulo.")
+    @Min(value = 0, message = "O Valor deve ser um número inteiro não negativo.")
+    private BigDecimal valor;
+}
